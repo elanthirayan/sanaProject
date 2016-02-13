09e3fdbb1ba89d19a6bf5758c328245b24d4909d
Kirtiman <kirtiman.patowary@axinovate.com>
2016-02-13 16:11:49 +0530

    detail page creation

---
+++ category_detail.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<?php ?>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<title>Category Detail Page</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<!--font awesome file-->
+		<link rel="stylesheet" href="css/font-awesome.css" type="text/css" >
+		
+	</head>
+
+	<body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="http://www.skilladda.com/assets/images/skilladda_logo180.png" style="width:110px;" /></a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Category 1</a></li>
+					<li><a href="#about">Category 2</a></li>
+					<li><a href="#contact">Category 3</a></li>
+					<li><a href="#contact">Category 4</a></li>
+					<!--li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					  <ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li role="separator" class="divider"></li>
+						<li class="dropdown-header">Nav header</li>
+						<li><a href="#">Separated link</a></li>
+						<li><a href="#">One more separated link</a></li>
+					  </ul>
+					</li-->
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class=""><a href="#">Login <span class="sr-only">(current)</span></a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+    </nav>
+
+    <div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">	
+				<div class="category-name">Yuvraj Singh's six 6s in an over.</div>
+				<hr class="br-grey mt-5"></hr>
+			</div>
+			<div class="col-md-5 col-sm-6 col-xs-12">
+				<video width="320" height="240" controls>
+					<source src="https://www.youtube.com/watch?v=dwH34mR6ZG8" type="video/mp4">
+					<source src="movie.ogg" type="video/ogg">
+					Your browser does not support the video tag.
+				</video>
+				
+			<!--	https://www.youtube.com/watch?v=dwH34mR6ZG8		-->
+			</div>
+			<div class="col-md-7 col-sm-6 col-xs-12">
+				<div class="text-head">
+					Indian batsman Yuvraj Singh is known to be one of the most flamboyant cricketers in the modern era. Yuvraj went all guns blazing when he sent Stuart Broad for six ruthless maximums in an over during the World T20 in 2007 against England. A somewhat ugly spat with Andrew Flintoff in that very game spurred Yuvi on as he dispatched the England fast bowler to almost all corners of the park.
+					<br/>
+					Yuvraj amazed cricketing fans worldwide with his six sixes in an over as he became the second batsman in international cricket and the fourth overall to reach this feat. India went on to win the World T20 that year thanks to some other similarly brilliant knocks by Yuvi.
+				</div>
+			</div>
+		</div>
+      
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  
+
+	</body>
+</html>
