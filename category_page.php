6e8f5bcde71a668a03081b58d9a806b964e84111
Kirtiman <kirtiman.patowary@axinovate.com>
2016-02-13 15:58:25 +0530

    category page added

---
+++ category_page.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<?php ?>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<title>Category Page</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<!--font awesome file-->
+		<link rel="stylesheet" href="css/font-awesome.css" type="text/css" >
+		
+	</head>
+
+	<body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="http://www.skilladda.com/assets/images/skilladda_logo180.png" style="width:110px;" /></a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Category 1</a></li>
+					<li><a href="#about">Category 2</a></li>
+					<li><a href="#contact">Category 3</a></li>
+					<li><a href="#contact">Category 4</a></li>
+					<!--li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					  <ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li role="separator" class="divider"></li>
+						<li class="dropdown-header">Nav header</li>
+						<li><a href="#">Separated link</a></li>
+						<li><a href="#">One more separated link</a></li>
+					  </ul>
+					</li-->
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class=""><a href="#">Login <span class="sr-only">(current)</span></a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+    </nav>
+
+    <div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">	
+				<div class="category-name">Weekend Destinations</div>
+				<hr class="br-grey mt-5"></hr>
+			</div>
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="row">
+					<div class="col-md-3 col-sm-4 col-xs-12">
+						<div class="product-box">
+							<div class="text-center">
+								<img src="images/pic1.jpg" class="product-img" />
+							</div>
+							<div class="text-head">
+								Network Engineer Network Engineer Network Engineer
+							</div>
+							<a href="javascript:void(0)" class="detail-btn">Know More <i class="fa fa-arrow-right"></i></a>
+						</div>
+						
+					</div>
+					<div class="col-md-3 col-sm-4 col-xs-12">
+						<div class="product-box">
+							<div class="text-center">
+								<img src="images/pic2.jpg" class="product-img" />
+							</div>
+							<div class="text-head">
+								Network Engineer 
+							</div>
+							<a href="javascript:void(0)" class="detail-btn">Know More <i class="fa fa-arrow-right"></i></a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4 col-xs-12">
+						<div class="product-box">
+							<div class="text-center">
+								<img src="images/pic3.jpg" class="product-img" />
+							</div>
+							<div class="text-head">
+								Network Engineer 
+							</div>
+							<a href="javascript:void(0)" class="detail-btn">Know More <i class="fa fa-arrow-right"></i></a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4 col-xs-12">
+						<div class="product-box">
+							<div class="text-center">
+								<img src="images/pic4.jpg" class="product-img" />
+							</div>
+							<div class="text-head">
+								Network Engineer 
+							</div>
+							<a href="javascript:void(0)" class="detail-btn">Know More <i class="fa fa-arrow-right"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+      
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  
+
+	</body>
+</html>
